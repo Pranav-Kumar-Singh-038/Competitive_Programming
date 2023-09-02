eb24b1a11a8d844f9c48bbf21fa478eaeb270377
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-02 20:36:54 +0530

    easy math problem

---
+++ Striver_CP_sheet/Implementation/Mahmoud_and_Ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Easiest I ever did
+//TC: O(1)
+//SC:O(1)
+int main()
+{
+    long int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Mahmoud";
+    }
+    else
+    {
+        cout<<"Ehab";
+    }
+}
