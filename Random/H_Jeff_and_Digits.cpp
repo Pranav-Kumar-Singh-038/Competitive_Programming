33ef6ccd74b88b1916641381d91a1dad5467d311
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-23 23:56:01 +0530

    Streak

---
+++ Random/H_Jeff_and_Digits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> vec)
+{
+  cout<<0;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> vec;
+    while(n--)
+    {
+        int x;
+        cin>>x;
+        vec.push_back(x);
+    }
+    solve(vec);
+}
