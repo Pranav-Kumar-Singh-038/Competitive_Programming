7329d6b07c2976b4c13d6ec3eee6d1d908b53789
Sleepy_Sage <kumarharsh073@gmail.com>
2023-10-07 20:44:36 +0530

    HnCC set complete

--- Random/J_Effective_Approach.cpp
+++ Random/J_Effective_Approach.cpp
@@ -1,55 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// void solve(vector<int> vec, vector<int> queries)
-// {
-//     unordered_map<int, int> index_map;
-//     int n = vec.size();
-
-//     for (int i = 0; i < n; ++i) {
-//         index_map[vec[i]] = i + 1;
-//     }
-
-//     int a = 0, b = 0;
-//     for (auto it : queries) {
-//         if (index_map.find(it) != index_map.end()) {
-//             a += index_map[it];
-//             b += n - index_map[it] + 1;
-//         }
-//     }
-
-//     cout << a << " " << b << endl;
-// }
-
-void solve(std::vector<int> vec, std::vector<int> queries)
+void solve(vector<int> vec, vector<int> queries)
 {
-    int a = 0, b = 0;
+    unordered_map<int, int> index_map;
     int n = vec.size();
-    for (auto it : queries)
-    {
-        int i = 0;
-        while (true)
-        {
-            if (vec[i] == it)
-            {
-                a += i + 1;
-                break;
-            }
-            i++;
-        }
-        int j = n - 1;
-        while (true)
-        {
-            if (vec[j] == it)
-            {
-                b += n - j;
-                break;
-            }
-            j--;
+
+    for (int i = 0; i < n; ++i) {
+        index_map[vec[i]] = i + 1;
+    }
+
+    long long int a = 0, b = 0;
+    for (auto it : queries) {
+        if (index_map.find(it) != index_map.end()) {
+            a += index_map[it];
+            b += n - index_map[it] + 1;
         }
     }
-    std::cout << a << " " << b << std::endl;
+
+    cout << a << " " << b << endl;
 }
+// Notice that the range of number of elements and the values of elements is the same
+// i guess the all the inputs will contains all the inputs till n, in random order
+// void solve(std::vector<int> vec, std::vector<int> queries)
+// {
+//     int a = 0, b = 0;
+//     int n = vec.size();
+//     for (auto it : queries)
+//     {
+//         int i = 0;
+//         while (true)
+//         {
+//             if (vec[i] == it)
+//             {
+//                 a += i + 1;
+//                 break;
+//             }
+//             i++;
+//         }
+//         int j = n - 1;
+//         while (true)
+//         {
+//             if (vec[j] == it)
+//             {
+//                 b += n - j;
+//                 break;
+//             }
+//             j--;
+//         }
+//     }
+//     std::cout << a << " " << b << std::endl;
+// }
 
 int main()
 {
