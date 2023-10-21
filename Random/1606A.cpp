0e25f07db618459b1308606dfb4027c5b0120795
Sleepy_Sage <kumarharsh073@gmail.com>
2023-10-21 15:35:48 +0530

    AB Balance

--- Random/1606A.cpp
+++ Random/1606A.cpp
@@ -1,6 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+Let's look at the first and the last characters of s
+. Note that if s1=sn
+ (where n=|s|
+), then AB(s)
+ is always equal to BA(s)
+.
 
+It can be proved, for example, by induction: if s
+ consists of equal characters then AB(s)=BA(s)=0
+; if s
+ has a structure like abb...ba (or baa...ab) then AB(s)=BA(s)=1
+.
+
+Otherwise, there is at least one character si
+ in the middle that equal to s1
+ and sn
+. So we can split string s
+ in s[1..i]
+ and s[i..n]
+. Both these string has AB=BA
+ (by induction), so our string s
+ also has AB(s)=BA(s)
+.
+
+As a result, if s1=sn
+ then the answer is 0
+, and we print the string untouched. Otherwise, we replace either s1
+ or sn
+ and get the desired string.
+
+(It also can be proved that if s1≠sn
+ then AB(s)≠BA(s)
+)
+*/
+//Solved it, with the helped of tutorial
 void solve()
 {
     string s;
@@ -27,28 +62,14 @@ void solve()
         arr2.push_back(pos);
         pos += ba.length();
     }
-   // cout << count1 << " " << count2 << endl;
     if(count1==count2)
     {
         cout<<s<<endl;
     }
-    else if(count1>count2)
+    else
     {
-        while(count1!=count2)
-        {
-            pos=s.find(ab);
-            s[pos]='b';
-            count1--;
-        }
-        cout<<s<<endl;
-    }
-    else{
-        while(count2!=count1)
-        {
-            pos=s.find(ba);
-            s[pos]='a';
-            count2--;
-        }
+        s[0]='a';
+        s[s.size()-1]='a';
         cout<<s<<endl;
     }
     return;
