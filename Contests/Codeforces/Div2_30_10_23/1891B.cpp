c2fe64219748e88dfe679c498a1b6d4d6d575c78
Sleepy_Sage <kumarharsh073@gmail.com>
2023-11-04 16:55:46 +0530

    upsolved Deja Vu

--- Contests/Codeforces/Div2_30_10_23/1891B.cpp
+++ Contests/Codeforces/Div2_30_10_23/1891B.cpp
@@ -1,74 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Third Approach
-// TC: O(n^2)
-// SC: O(1)
-//Streak
-void solve()
-{
-    long long int n, q;
-    cin >> n >> q;
-    vector<long long int> nums;
-    while (n--)
-    {
-        long long int x;
-        cin >> x;
-        nums.push_back(x);
-    }
-    vector<long long int> vec;
-    while (q--)
-    {
-        long long int x;
-        cin >> x;
-        vec.push_back(x);
-    }
-
-    vector<long long int> powers_of_2(32);
-    powers_of_2[0] = 1;
-    for (int i = 1; i < 32; i++)
-    {
-        powers_of_2[i] = 2 * powers_of_2[i - 1];
-    }
-
-    long long int min = *min_element(nums.begin(), nums.end());
-    long long int max = *max_element(nums.begin(), nums.end());
-    long long int small = vec[0];
-    for (long long int i = 0; i < vec.size(); i++)
-    {
-        if (vec[i] > small)
-        {
-            continue;
-        }
-        else
-        {
-            long long int power = powers_of_2[vec[i]];
-            long long int sub = powers_of_2[vec[i] - 1];
-            for (long long int j = 0; j < nums.size(); j++)
-            {
-                if (power > max || nums[j] % 2 != 0)
-                {
-                    continue;
-                }
-                else if (nums[j] % power == 0)
-                {
-                    nums[j] += sub;
-                }
-            }
-            small = vec[i];
-        }
-    }
-    for (auto it : nums)
-    {
-        cout << it << " ";
-    }
-    cout << endl;
-}
-
-// First Approach
-// O(n^2)
-// O(1)
-// TLE
+// Third Approach
+//  TC: O(n^2)
+//  SC: O(1)
+// Streak
 // void solve()
 // {
 //     long long int n, q;
@@ -87,23 +23,30 @@ void solve()
 //         cin >> x;
 //         vec.push_back(x);
 //     }
-//     long long int min = *min_element(nums.begin(), nums.end());
-//     long long int max = *max_element(nums.begin(), nums.end());
 
+//     vector<long long int> powers_of_2(32);
+//     powers_of_2[0] = 1;
+//     for (int i = 1; i < 32; i++)
+//     {
+//         powers_of_2[i] = 2 * powers_of_2[i - 1];
+//     }
+
+//     long long int small = vec[0];
 //     for (long long int i = 0; i < vec.size(); i++)
 //     {
-//         long long int power = pow(2, vec[i]);
-//         long long int sub = pow(2, vec[i] - 1);
-//         for (long long int j = 0; j < nums.size(); j++)
+
+//         long long int power = powers_of_2[vec[i]];
+//         long long int sub = powers_of_2[vec[i] - 1];
+//         if (vec[i] < small)
 //         {
-//             if (power > max || nums[j]%2!=0)
+//             for (long long int j = 0; j < nums.size(); j++)
 //             {
-//                 continue;
+//                 if (nums[j] % power == 0)
+//                 {
+//                     nums[j] += sub;
+//                 }
 //             }
-//             else if (nums[j] % power == 0)
-//             {
-//                 nums[j] += sub;
-//             }
+//             small = vec[i];
 //         }
 //     }
 //     for (auto it : nums)
@@ -112,6 +55,50 @@ void solve()
 //     }
 //     cout << endl;
 // }
+
+// Final Approach
+// O(n^2)
+// O(1)
+// TLE
+void solve()
+{
+    long long int n, q;
+    cin >> n >> q;
+    vector<long long int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    vector<long long int> powers_of_2(32);
+    powers_of_2[0] = 1;
+    for (int i = 1; i < 32; i++)
+    {
+        powers_of_2[i] = 2 * powers_of_2[i - 1];
+    }
+    int min = INT_MAX;
+    while (q--)
+    {//
+        long long int x;
+        cin >> x;
+        if (x <= min)
+        {
+        long long int power = powers_of_2[x];
+        long long int sub = powers_of_2[x - 1];
+            for (long long int j = 0; j < nums.size(); j++)
+            {
+                if (nums[j] % power == 0)
+                {
+                    nums[j] += sub;
+                }
+            }
+            min = x;
+        }
+    }
+    for (auto it : nums)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+}
 
 // Second Approach
 //  TC: O(n^2)
