7c8450e8856e7f8c3e15981451c54094ab68bbfd
Sleepy_Sage <kumarharsh073@gmail.com>
2023-11-06 17:14:42 +0530

    Lucky ticket

--- Random/1895C.cpp
+++ Random/1895C.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int check_lucky(string x)
-{
-    int first_half = 0;
-    int second_half = 0;
+int check_lucky(const string &x) {
+    int first_half_sum = 0;
+    int second_half_sum = 0;
+    int length = x.size();
+    
+    for (int i = 0; i < length; i++) {
+        if (i < length / 2) {
+            first_half_sum += x[i] - '0';
+        } else {
+            second_half_sum += x[i] - '0';
+        }
+    }
 
-    for (int i = 0; i < (x.size() / 2); i++)
-    {
-        first_half += (x[i] - '0');
-    }
-    for (int i = (x.size() / 2); i < x.size(); i++)
-    {
-        second_half += (x[i] - '0');
-    }
-    if (first_half == second_half)
-    {
-        return 1;
-    }
-    return 0;
+    return (first_half_sum == second_half_sum) ? 1 : 0;
 }
+
 
 void solve()
 {
@@ -51,6 +48,44 @@ void solve()
     }
     cout << count + n;
 }
+
+// void solve()
+// {
+//     int n;
+//     cin >> n;
+//     vector<string> ticketpieces;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         string x;
+//         cin >> x;
+//         ticketpieces.push_back(x);
+//     }
+//     int count = 0;
+//     vector<string> possible;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+
+//         string x;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if ((ticketpieces[i].size() % 2 == 0 && ticketpieces[j].size() % 2 == 0) || (ticketpieces[i].size() % 2 != 0 && ticketpieces[j].size() % 2 != 0))
+//             {
+//                 x = ticketpieces[i] + ticketpieces[j];
+//                 possible.push_back(x);
+//                 // count += check_lucky(x);
+//                 x = ticketpieces[j] + ticketpieces[i];
+//                 // count += check_lucky(x);
+//                 possible.push_back(x);
+//             }
+//         }
+//     }
+//     for (auto it : possible)
+//     {
+//         count += check_lucky(it);
+//     }
+//     cout << count + n;
+// }
 
 int main()
 {
