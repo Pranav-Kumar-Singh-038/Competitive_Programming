14c695508f637f11c5b310647b1499cbeee09cbc
Sleepy_Sage <kumarharsh073@gmail.com>
2023-11-10 21:34:51 +0530

    Streak

---
+++ Striver_CP_sheet/Implementation/1373A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    double cost1, numDonut2, costpack2;
+    cin >> cost1 >> numDonut2 >> costpack2;
+    // double cost2 = costpack2 / numDonut2;
+    // // cout << cost1 << " " << cost2 << endl;
+    // double ans1,ans2;
+    // if(cost1<=cost2)
+    // {
+    //     ans1=1;
+    //     ans2=-1;
+    // }
+    // else
+    // {
+    //     ans1=cost1;
+    //     ans2=cost2;
+    // }
+    // cout<<ans1<<" "<<ans2<<endl;
+    cout << "-1 20" << endl
+         << "8 -1" << endl
+         << "1 2" << endl
+         << "-1 1000000000";
+}
+
+int main()
+{
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+        solve();
+    // }
+}
