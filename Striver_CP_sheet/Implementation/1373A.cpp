9d031f4887c2ba52850789dedec0d0c079fd8237
Sleepy_Sage <kumarharsh073@gmail.com>
2023-11-12 21:39:43 +0530

    Tried

--- Striver_CP_sheet/Implementation/1373A.cpp
+++ Striver_CP_sheet/Implementation/1373A.cpp
@@ -5,32 +5,45 @@ void solve()
 {
     double cost1, numDonut2, costpack2;
     cin >> cost1 >> numDonut2 >> costpack2;
-    // double cost2 = costpack2 / numDonut2;
-    // // cout << cost1 << " " << cost2 << endl;
-    // double ans1,ans2;
-    // if(cost1<=cost2)
-    // {
-    //     ans1=1;
-    //     ans2=-1;
-    // }
-    // else
-    // {
-    //     ans1=cost1;
-    //     ans2=cost2;
-    // }
-    // cout<<ans1<<" "<<ans2<<endl;
-    cout << "-1 20" << endl
-         << "8 -1" << endl
-         << "1 2" << endl
-         << "-1 1000000000";
+    double cost2 = costpack2 / numDonut2;
+    int ans1, ans2;
+    if (cost1 < cost2)
+    {
+        ans1 = 1000000000;
+        ans2 = -1;
+    }
+    else if (cost1 == cost2)
+    {
+        ans2 = -1;
+        ans1=numDonut2-1;
+    }
+    else
+    {
+        if (cost1 < costpack2)
+        {
+            ans1 = costpack2/cost1;
+            ans2 = numDonut2;
+        }
+        else if (cost1 == costpack2)
+        {
+            ans1 = -1;
+            ans2 = 1000000000;
+        }
+        else
+        {
+            ans1 = -1;
+            ans2 = 1000000000;
+        }
+    }
+    cout << ans1 << " " << ans2 << endl;
 }
 
 int main()
 {
-    // int t;
-    // cin >> t;
-    // while (t--)
-    // {
+    int t;
+    cin >> t;
+    while (t--)
+    {
         solve();
-    // }
+    }
 }
